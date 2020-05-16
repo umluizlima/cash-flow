98f2b821b94c7fea37b52ac5d4f1f7642699c0fe
Luiz Lima <umluizlima@gmail.com>
2020-05-16 20:08:23 -0300

    Add database module

---
+++ app/core/database.py
@@ -0,0 +1,26 @@
+from os import getenv
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def get_url():
+    return getenv(
+        "DATABASE_URL", default="postgresql://postgres:postgres@localhost/cash-flow",
+    )
+
+
+engine = create_engine(get_url())
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db():
+    try:
+        session = Session()
+        yield session
+        session.commit()
+    except Exception as error:
+        session.rollback()
+        raise error
+    finally:
+        session.close()
